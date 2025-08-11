a6725ddad666d0b703dafa12bf89384e5f699a93
falcaodaniella-gif <falcaodaniella@gmail.com>
2025-08-11 19:09:02 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+# Adiciona o diretório pai ao PYTHONPATH para que as importações funcionem
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+
 from src.main import create_app
 
-app = create_app( )
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
