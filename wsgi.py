aa543f14691688cb164339a09b531fe79fb16824
falcaodaniella-gif <falcaodaniella@gmail.com>
2025-08-11 19:20:12 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-# Adiciona o diretório pai ao PYTHONPATH para que as importações funcionem
+# Adiciona o diretório raiz do projeto ao PYTHONPATH
+# Isso permite que o Python encontre o módulo 'src.main'
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 from src.main import create_app
