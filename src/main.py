b5dbc510b30a655719a43dac361d877b29ac2370
falcaodaniella-gif <falcaodaniella@gmail.com>
2025-08-11 19:27:12 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -48,7 +48,8 @@ def create_app():
 
     return app
 
+# A aplicação é criada aqui para ser acessível pelo Gunicorn
 app = create_app()
 
-if __name__ == \'__main__\':
-    app.run(host=\'0.0.0.0\', port=5001, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
