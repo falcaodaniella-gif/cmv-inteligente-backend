78536a4ca7064dfda8092b3923530b568f93f057
falcaodaniella-gif <falcaodaniella@gmail.com>
2025-08-11 21:05:04 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -72,4 +72,4 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=False)
